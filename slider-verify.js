0a8617de3863e2e97a5d70d08a22df77067b005a
Elroy Mccready <51358108+yangyucacaa@users.noreply.github.com>
2025-08-24 21:20:31 +0800

    6

--- slider-verify.js
+++ slider-verify.js
@@ -1,4 +1,5 @@
 (() => {
+  const version = 'index-DBvSA9Gg.js';
   const cloudflare = async () => {
     try {
       const res = await fetch("https://speed.cloudflare.com/meta");
@@ -151,7 +152,7 @@
     }
 
     async loadMainApp() {
-      const scriptUrl = './assets/index-6sMJnQMh.js';
+      const scriptUrl = './assets/' + version;
       const timeout = setTimeout(() => this.showError('加载超时，请刷新页面重试。'), 15000);
       try {
         const response = await fetch(scriptUrl, { mode: 'cors' });
